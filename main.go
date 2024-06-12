20d40faeda00b8e802c9e18f39acff9bf8a7e207
earl86 <wang_chao86@126.com>
2024-06-12 14:15:57 +0800

    add modify some code

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ var (
 	Version = "DEV"
 
 	// We pass credentials in this env var as there is no better way of doing this from haproxy.
-	mysqlCredentials   = os.Getenv("PATH")
-	mysqlAddress       = os.Getenv("HAPROXY_SERVER_ADDR") + ":" + os.Getenv("HAPROXY_SERVER_PORT")
-	haproxyBackendName = os.Getenv("HAPROXY_PROXY_NAME")
+	//mysqlCredentials   = os.Getenv("PATH")
+	//haproxyBackendName = os.Getenv("HAPROXY_PROXY_NAME")
 )
 
 type eventRow struct {
@@ -31,6 +30,8 @@ type eventRow struct {
 	ReadOnly           string
 	TransactionsBehind string
 	TransactionsToCert string
+	MemberRole         string
+	MemberState        string
 }
 
 func debugMsg(isDebug bool, msg string) {
@@ -41,8 +42,16 @@ func debugMsg(isDebug bool, msg string) {
 
 func main() {
 	var versionFlag, debugFlag bool
+	var mysql_ip, mysql_username, mysql_password, haproxyBackendName string
+	var mysql_port int
 	flag.BoolVar(&versionFlag, "v", false, "show version")
 	flag.BoolVar(&debugFlag, "d", false, "enable debug output")
+	flag.StringVar(&mysql_ip, "n", "", "mysql host ip")
+	flag.IntVar(&mysql_port, "p", 3306, "mysql host port")
+	flag.StringVar(&mysql_username, "u", "", "mysql username")
+	flag.StringVar(&mysql_password, "a", "", "mysql password")
+	flag.StringVar(&haproxyBackendName, "x", "", "haproxyBackendName")
+
 	flag.Parse()
 	if versionFlag {
 		fmt.Println("Version", Version)
@@ -54,12 +63,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/?%s", mysqlCredentials, mysqlAddress, mysqlParams))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/?%s", mysql_username, mysql_password, mysql_ip, mysql_port, mysqlParams))
 	if err != nil {
 		fmt.Println("Error connecting to MySQL", err)
 		os.Exit(1)
 	}
 
+	//| viable_candidate | read_only | transactions_behind | transactions_to_cert | member_role | member_state |
 	rows, err := db.Query(query)
 	if err != nil {
 		fmt.Println("Error selecting from MySQL table:", err)
@@ -68,7 +78,7 @@ func main() {
 
 	var row eventRow
 	for rows.Next() {
-		rows.Scan(&row.ViableCandidate, &row.ReadOnly, &row.TransactionsBehind, &row.TransactionsToCert)
+		rows.Scan(&row.ViableCandidate, &row.ReadOnly, &row.TransactionsBehind, &row.TransactionsToCert, &row.MemberRole, &row.MemberState)
 		debugMsg(debugFlag, fmt.Sprintf("MySQL query result: %+v\n", row))
 		break
 	}
@@ -78,10 +88,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	if strings.HasSuffix(haproxyBackendName, "_primary") && row.ReadOnly == "NO" {
+	if strings.HasSuffix(haproxyBackendName, "_primary") && row.ReadOnly == "NO" && row.MemberRole == "PRIMARY" && row.MemberState == "ONLINE" {
 		debugMsg(debugFlag, "HEALTHCHECK PRIMARY - OK")
 		return
-	} else if strings.HasSuffix(haproxyBackendName, "_secondary") && row.ReadOnly == "YES" {
+	} else if strings.HasSuffix(haproxyBackendName, "_secondary") && row.ReadOnly == "YES" && row.MemberRole == "SECONDARY" && row.MemberState == "ONLINE" {
 		debugMsg(debugFlag, "HEALTHCHECK SECONDARY - OK")
 		return
 	}
